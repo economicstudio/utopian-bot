93a81afb186a39c1899f51320b827e9c86e56728
amosbastian <amosbastian@gmail.com>
2018-08-11 20:08:45 +0100

    update: now uses constants from constants.py

--- unvote_bot.py
+++ unvote_bot.py
@@ -1,52 +1,10 @@
-from beem import Steem
 from beem.account import Account
 from beem.comment import Comment, RecentReplies
-from datetime import date, datetime, timedelta
-from dateutil.parser import parse
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
+from datetime import timedelta
+import constants
 import json
-import logging
 import os
 import time
-
-# Get path of current folder
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-# Logging
-logger = logging.getLogger("utopian-io")
-logger.setLevel(logging.INFO)
-fh = logging.FileHandler(f"{DIR_PATH}/bot.log")
-fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-
-# Beem
-steem = Steem()
-ACCOUNT = "utopian-io"
-
-# Spreadsheet variables
-scope = ["https://spreadsheets.google.com/feeds",
-         "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    f"{DIR_PATH}/client_secret.json", scope)
-client = gspread.authorize(credentials)
-sheet = client.open("Utopian Reviews")
-
-# Get date of current, next and previous Thursday
-today = date.today()
-offset = (today.weekday() - 3) % 7
-this_week = today - timedelta(days=offset)
-last_week = this_week - timedelta(days=7)
-next_week = this_week + timedelta(days=7)
-
-# Get title's of most recent two worksheets
-title_previous = f"Reviewed - {last_week:%b %-d} - {this_week:%b %-d}"
-title_current = f"Reviewed - {this_week:%b %-d} - {next_week:%b %-d}"
-previous_reviewed = sheet.worksheet(title_previous)
-current_reviewed = sheet.worksheet(title_current)
 
 
 def get_replies(post):
@@ -54,7 +12,7 @@ def get_replies(post):
     Gets all replies to the given post.
     """
     url = f"{post.category}/{post.authorperm}"
-    return steem.rpc.get_state(url)["content"].keys()
+    return constants.STEEM.rpc.get_state(url)["content"].keys()
 
 
 def update_comment(post):
@@ -62,22 +20,17 @@ def update_comment(post):
     Updates the comment left by the bot to reflect that the contribution was
     unvoted.
     """
-    body = (f"Hey @{post.author}, your contribution was unvoted because we "
-            "found out that it did not follow the "
-            "[Utopian rules](https://join.utopian.io/rules).\n\n Upvote this "
-            "comment to help Utopian grow its power and help other Open "
-            "Source contributions like this one.\n\n**Want to chat? Join us "
-            "on [Discord](https://discord.gg/Pc8HG9x).**")
+    body = constants.COMMENT_UNVOTE.format(post.author)
 
     # Iterate over replies until bot's comment is found
     for comment in get_replies(post):
         comment = Comment(comment)
-        if comment.author == ACCOUNT and comment.is_comment():
+        if comment.author == constants.ACCOUNT and comment.is_comment():
             try:
-                logger.info(f"Updating comment {comment.authorperm}")
+                constants.LOGGER.info(f"Updating comment {comment.authorperm}")
                 comment.edit(body, replace=True)
             except Exception as error:
-                logger.error(error)
+                constants.LOGGER.error(error)
             return
 
 
@@ -86,30 +39,26 @@ def unvote_post(row, previous, current):
     Unvotes the given post and updates the spreadsheet to reflect this.
     """
     url = row[2]
-    account = Account(ACCOUNT)
-    post = Comment(url, steem_instance=steem)
+    account = Account(constants.ACCOUNT)
+    post = Comment(url, steem_instance=constants.STEEM)
 
-    # Check if actually voted on post
     votes = [vote["voter"] for vote in post.json()["active_votes"]]
-    if ACCOUNT not in votes:
-        logger.info(f"Never voted on {url} in the first place!")
+    if constants.ACCOUNT not in votes:
+        constants.LOGGER.info(f"Never voted on {url} in the first place!")
         return
 
     # Unvote the post
     try:
-        logger.info(f"Unvoting {url}")
+        constants.LOGGER.info(f"Unvoting {url}")
         post.vote(0, account=account)
         time.sleep(3)
-        # Edit comment
         update_comment(post)
     except Exception as error:
-        logger.error(error)
+        constants.LOGGER.error(error)
 
-    # Just in case sheet was updated in the meantime
-    previous_reviewed = sheet.worksheet(title_previous)
-    current_reviewed = sheet.worksheet(title_current)
+    previous_reviewed = constants.SHEET.worksheet(constants.TITLE_PREVIOUS)
+    current_reviewed = constants.SHEET.worksheet(constants.TITLE_CURRENT)
 
-    # Update row depending on which sheet it's in
     if row in previous:
         row_index = previous.index(row) + 1
         previous_reviewed.update_cell(row_index, 10, "Unvoted")
@@ -126,13 +75,13 @@ def main():
     necessary.
     """
     # Get data from both the current sheet and previous one
-    previous = previous_reviewed.get_all_values()
-    current = current_reviewed.get_all_values()
+    previous = constants.PREVIOUS_REVIEWED.get_all_values()
+    current = constants.CURRENT_REVIEWED.get_all_values()
     reviewed = previous[1:] + current[1:]
 
     # If file already exists compare with current data
-    if os.path.isfile(f"{DIR_PATH}/reviews.json"):
-        with open(f"{DIR_PATH}/reviews.json") as fd:
+    if os.path.isfile(f"{constants.DIR_PATH}/reviews.json"):
+        with open(f"{constants.DIR_PATH}/reviews.json") as fd:
             data = json.load(fd)
         for row in reviewed:
             # Row has been changed
@@ -144,7 +93,7 @@ def main():
                     unvote_post(row, previous, current)
 
     # Save current data
-    with open(f"{DIR_PATH}/reviews.json", "w") as fd:
+    with open(f"{constants.DIR_PATH}/reviews.json", "w") as fd:
         json.dump(reviewed, fd, indent=4)
 
 if __name__ == '__main__':
--- upvote_bot.py
+++ upvote_bot.py
@@ -1,80 +1,9 @@
-from beem import Steem
 from beem.account import Account
 from beem.comment import Comment
-from datetime import date, datetime, timedelta
+from datetime import timedelta
 from dateutil.parser import parse
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
-import logging
-import os
+import constants
 import requests
-
-# Get path of current folder
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-# The minimum age a post must be to receive an upvote
-MINIMUM_AGE = 24
-
-# Logging
-logger = logging.getLogger("utopian-io")
-logger.setLevel(logging.INFO)
-fh = logging.FileHandler(f"{DIR_PATH}/bot.log")
-fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-
-# Beem
-steem = Steem()
-ACCOUNT = "utopian-io"
-
-# Spreadsheet variables
-scope = ["https://spreadsheets.google.com/feeds",
-         "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    f"{DIR_PATH}/client_secret.json", scope)
-client = gspread.authorize(credentials)
-sheet = client.open("Utopian Reviews")
-
-# Get date of current, next and previous Thursday
-today = date.today()
-offset = (today.weekday() - 3) % 7
-this_week = today - timedelta(days=offset)
-last_week = this_week - timedelta(days=7)
-next_week = this_week + timedelta(days=7)
-
-# Get title's of most recent two worksheets
-title_previous = f"Reviewed - {last_week:%b %-d} - {this_week:%b %-d}"
-title_current = f"Reviewed - {this_week:%b %-d} - {next_week:%b %-d}"
-previous_reviewed = sheet.worksheet(title_previous)
-current_reviewed = sheet.worksheet(title_current)
-
-MAX_VOTE = {
-    "ideas": 20.0,
-    "development": 55.0,
-    "bug-hunting": 13.0,
-    "translations": 35.0,
-    "graphics": 40.0,
-    "analysis": 45.0,
-    "social": 30.0,
-    "documentation": 30.0,
-    "tutorials": 30.0,
-    "video-tutorials": 35.0,
-    "copywriting": 30.0,
-    "blog": 30.0,
-}
-MAX_TASK_REQUEST = 6.0
-
-DAVINCI_BENEFICIARY = {
-    "account": "davinci.pay",
-    "weight": 1000
-}
-
-UTOPIAN_BENEFICIARY = {
-    "account": "utopian.pay",
-    "weight": 500
-}
 
 
 def bot_comment(post, category, account, staff_picked=False):
@@ -88,26 +17,19 @@ def bot_comment(post, category, account, staff_picked=False):
     else:
         contribution = "contribution"
 
-    body = (f"Hey @{post.author}\n"
-            "**Thanks for contributing on Utopian**.\n")
+    body = constants.COMMENT_HEADER.format(post.author)
 
     # If staff picked add text
     if staff_picked:
-        body += ("Congratulations! Your contribution was Staff Picked to "
-                 f"receive a maximum vote for the {category} category on "
-                 "Utopian for being of significant value to the project and "
-                 "the open source community.\n\n")
+        body += constants.COMMENT_STAFF_PICK.format(category)
 
-    body += (f"We’re already looking forward to your next {contribution}!\n\n"
-             "**Want to chat? Join us on Discord https://discord.gg/h52nFrV.**"
-             "\n\n<a href='https://v2.steemconnect.com/sign/account-witness-v"
-             "ote?witness=utopian-io&approve=1'>Vote for Utopian Witness!</a>")
+    body += constants.COMMENT_FOOTER.format(contribution)
 
-    logger.info(f"Commenting on {post.authorperm} - {body}")
+    constants.LOGGER.info(f"Commenting on {post.authorperm} - {body}")
     try:
-        post.reply(body, author=ACCOUNT)
+        post.reply(body, author=constants.ACCOUNT)
     except Exception as comment_error:
-        logger.error(comment_error)
+        constants.LOGGER.error(comment_error)
 
 
 def valid_age(post):
@@ -123,8 +45,8 @@ def valid_translation(post):
     """
     Checks if a translation has the correct beneficiaries set.
     """
-    if (DAVINCI_BENEFICIARY in post.json()["beneficiaries"] and
-        UTOPIAN_BENEFICIARY in post.json()["beneficiaries"]):
+    if (constants.DAVINCI_BENEFICIARY in post.json()["beneficiaries"] and
+        constants.UTOPIAN_BENEFICIARY in post.json()["beneficiaries"]):
         return True
     return False
 
@@ -133,7 +55,7 @@ def beneficiary(post):
     """
     Checks if utopian.pay set as 5% beneficiary
     """
-    if (UTOPIAN_BENEFICIARY in post.json()["beneficiaries"]):
+    if (constants.UTOPIAN_BENEFICIARY in post.json()["beneficiaries"]):
         return True
     return False
 
@@ -142,10 +64,9 @@ def update_sheet(row, previous, current, vote_status):
     """
     Updates the row in one of the eligible worksheets.
     """
-    # Just in case sheet was updated in the meantime
-    previous_reviewed = sheet.worksheet(title_previous)
-    current_reviewed = sheet.worksheet(title_current)
-    # Update row depending on which sheet it's in
+    previous_reviewed = constants.SHEET.worksheet(constants.TITLE_PREVIOUS)
+    current_reviewed = constants.SHEET.worksheet(constants.TITLE_CURRENT)
+
     if row in previous:
         row_index = previous.index(row) + 1
         previous_reviewed.update_cell(row_index, 10, vote_status)
@@ -159,9 +80,9 @@ def max_pct(category):
     Returns the max voting percentage of the given category.
     """
     try:
-        vote_pct = MAX_VOTE[category]
+        vote_pct = constants.MAX_VOTE[category]
     except KeyError:
-        vote_pct = MAX_TASK_REQUEST
+        vote_pct = constants.MAX_TASK_REQUEST
     return vote_pct
 
 
@@ -170,7 +91,7 @@ def update_pct(vote_pct):
     Updates the voting percentage if beneficiaries utopian.pay set.
     """
     vote_pct = float(vote_pct)
-    utopian_weight = UTOPIAN_BENEFICIARY["weight"]
+    utopian_weight = constants.UTOPIAN_BENEFICIARY["weight"]
     return vote_pct + 0.1 * vote_pct + utopian_weight / 100.0 + 1.0
 
 
@@ -180,7 +101,7 @@ def vote_update(row, previous, current, staff_picked=False):
     """
     url = row[2]
     category = row[4]
-    account = Account(ACCOUNT)
+    account = Account(constants.ACCOUNT)
 
     # Check if post was staff picked
     if staff_picked:
@@ -189,37 +110,37 @@ def vote_update(row, previous, current, staff_picked=False):
         vote_pct = float(row[-1])
 
     try:
-        post = Comment(url, steem_instance=steem)
+        post = Comment(url, steem_instance=constants.STEEM)
         
         # If in last twelve hours before payout don't vote
         if not valid_age(post):
-            logger.error(f"In last 12 hours before payout: {url}")
+            constants.LOGGER.error(f"In last 12 hours before payout: {url}")
             update_sheet(row, previous, current, "EXPIRED")
             return
 
         # Already voted on
         votes = [vote["voter"] for vote in post.json()["active_votes"]]
-        if ACCOUNT in votes:
-            logger.error(f"Already voted on: {url}")
+        if constants.ACCOUNT in votes:
+            constants.LOGGER.error(f"Already voted on: {url}")
             update_sheet(row, previous, current, "Already voted on!")
             return
 
         # Curation rewards turned off
         allows_curation = post.json()["allow_curation_rewards"]
         if not allows_curation:
-            logger.error(f"Does not allow curation rewards: {url}")
+            constants.LOGGER.error(f"Does not allow curation rewards: {url}")
             update_sheet(row, previous, current, "Doesn't allow curation!")
             return
         
         # Wrong translation beneficiaries set
         if category == "translations" and not valid_translation(post):
-            logger.error(f"Wrong translation beneficiaries: {url}")
+            constants.LOGGER.error(f"Wrong translation beneficiaries: {url}")
             update_sheet(row, previous, current, "Beneficiaries wrong!")
             return
         
         # Voting % higher than possible
         if vote_pct > max_pct(category):
-            logger.error(f"Voting % exceeds max: {url}")
+            constants.LOGGER.error(f"Voting % exceeds max: {url}")
             update_sheet(
                 row, previous, current, "Voting percentage exceeds max!")
             return
@@ -228,12 +149,12 @@ def vote_update(row, previous, current, staff_picked=False):
         if beneficiary_set:
             vote_pct = update_pct(vote_pct)
 
-        logger.info(f"Voting on {post.authorperm} with {vote_pct}%")
+        constants.LOGGER.info(f"Voting on {post.authorperm} with {vote_pct}%")
         post.vote(vote_pct, account=account)
         bot_comment(post, category, account, staff_picked)
         update_sheet(row, previous, current, "Yes")
     except Exception as vote_error:
-        logger.error(vote_error)
+        constants.LOGGER.error(vote_error)
 
 
 def main():
@@ -241,19 +162,19 @@ def main():
     If voting power is 99.75 then it votes on the oldest contribution currently
     pending.
     """
-    voting_power = Account(ACCOUNT).get_voting_power()
+    voting_power = Account(constants.ACCOUNT).get_voting_power()
     
-    logger.info(f"Current voting power: {voting_power}")
+    constants.LOGGER.info(f"Current voting power: {voting_power}")
     if voting_power < 99.75:
         return
 
-    previous = previous_reviewed.get_all_values()
-    current = current_reviewed.get_all_values()
+    previous = constants.PREVIOUS_REVIEWED.get_all_values()
+    current = constants.CURRENT_REVIEWED.get_all_values()
     rows = previous[1:] + current[1:]
 
     response = requests.get("https://utopian.rocks/api/posts?status=pending")
     if len(response.json()) == 0:
-        logger.info(f"No eligible posts older than {MINIMUM_AGE} hours found.")
+        constants.LOGGER.info(f"No pending contributions found in the queue.")
         return
 
     pending = sorted(response.json(), key=lambda x: x["created"]["$date"])
@@ -267,8 +188,8 @@ def main():
             url = row[2]
             # Contribution found in spreadsheet
             if url == contribution["url"]:
-                post = Comment(url, steem_instance=steem)
-                if post.time_elapsed() > timedelta(hours=MINIMUM_AGE):
+                post = Comment(url, steem_instance=constants.STEEM)
+                if post.time_elapsed() > timedelta(hours=constants.MINIMUM_AGE):
                     staff_picked = row[6].lower()
                     if staff_picked:
                         vote_update(row, previous, current, True)
