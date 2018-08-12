8f3acf8abb4c19e2b6dc8657f3bb7f73a41ceb27
amosbastian <amosbastian@gmail.com>
2018-08-12 20:35:54 +0100

    update: refactor & add comment upvote functionality

--- upvote_bot.py
+++ upvote_bot.py
@@ -1,9 +1,11 @@
 from beem.account import Account
 from beem.comment import Comment
+from contribution import Contribution
 from datetime import timedelta
 from dateutil.parser import parse
 import constants
 import requests
+import time
 
 
 def bot_comment(post, category, staff_picked=False):
@@ -60,54 +62,62 @@ def beneficiary(post):
     return False
 
 
-def update_sheet(row, previous, current, vote_status):
+def update_sheet(row, vote_status, contribution=True):
     """
     Updates the row in one of the eligible worksheets.
     """
+    row = list(row.__dict__.values())
+    previous, current, _ = get_rows()
+
+    if contribution:
+        column_index = 11
+    else:
+        column_index = 10
+
     previous_reviewed = constants.SHEET.worksheet(constants.TITLE_PREVIOUS)
     current_reviewed = constants.SHEET.worksheet(constants.TITLE_CURRENT)
 
     if row in previous:
         row_index = previous.index(row) + 1
-        previous_reviewed.update_cell(row_index, 10, vote_status)
+        previous_reviewed.update_cell(row_index, column_index, vote_status)
     elif row in current:
         row_index = current.index(row) + 1
-        current_reviewed.update_cell(row_index, 10, vote_status)
+        current_reviewed.update_cell(row_index, column_index, vote_status)
 
 
-def max_pct(category):
+def max_weight(category):
     """
     Returns the max voting percentage of the given category.
     """
     try:
-        vote_pct = constants.MAX_VOTE[category]
+        weight = constants.MAX_VOTE[category]
     except KeyError:
-        vote_pct = constants.MAX_TASK_REQUEST
-    return vote_pct
+        weight = constants.MAX_TASK_REQUEST
+    return weight
 
 
-def update_pct(vote_pct):
+def update_weight(weight):
     """
     Updates the voting percentage if beneficiaries utopian.pay set.
     """
-    vote_pct = float(vote_pct)
+    weight = float(weight)
     utopian_weight = constants.UTOPIAN_BENEFICIARY["weight"]
-    return vote_pct + 0.1 * vote_pct + utopian_weight / 100.0 + 1.0
+    return weight + 0.1 * weight + utopian_weight / 100.0 + 1.0
 
 
-def vote_update(row, previous, current, staff_picked=False):
+def vote_update(row, staff_picked=False):
     """
     Upvotes the highest priority contribution and updates the spreadsheet.
     """
-    url = row[2]
-    category = row[4]
+    url = row.url
+    category = row.category
     account = Account(constants.ACCOUNT)
 
     # Check if post was staff picked
     if staff_picked:
-        vote_pct = max_pct(category)
+        weight = max_weight(category)
     else:
-        vote_pct = float(row[-1])
+        weight = float(row.weight)
 
     try:
         post = Comment(url, steem_instance=constants.STEEM)
@@ -115,62 +125,129 @@ def vote_update(row, previous, current, staff_picked=False):
         # If in last twelve hours before payout don't vote
         if not valid_age(post):
             constants.LOGGER.error(f"In last 12 hours before payout: {url}")
-            update_sheet(row, previous, current, "EXPIRED")
+            update_sheet(row, "EXPIRED")
             return
 
         # Already voted on
         votes = [vote["voter"] for vote in post.json()["active_votes"]]
         if constants.ACCOUNT in votes:
             constants.LOGGER.error(f"Already voted on: {url}")
-            update_sheet(row, previous, current, "Already voted on!")
+            update_sheet(row, "Already voted on!")
             return
 
         # Curation rewards turned off
         allows_curation = post.json()["allow_curation_rewards"]
         if not allows_curation:
             constants.LOGGER.error(f"Does not allow curation rewards: {url}")
-            update_sheet(row, previous, current, "Doesn't allow curation!")
+            update_sheet(row, "Doesn't allow curation!")
             return
         
         # Wrong translation beneficiaries set
         if category == "translations" and not valid_translation(post):
             constants.LOGGER.error(f"Wrong translation beneficiaries: {url}")
-            update_sheet(row, previous, current, "Beneficiaries wrong!")
+            update_sheet(row, "Beneficiaries wrong!")
             return
         
         # Voting % higher than possible
-        if vote_pct > max_pct(category):
+        if weight > max_weight(category):
             constants.LOGGER.error(f"Voting % exceeds max: {url}")
-            update_sheet(
-                row, previous, current, "Voting percentage exceeds max!")
+            update_sheet(row, "Voting percentage exceeds max!")
             return
         
         beneficiary_set = beneficiary(post)
         if beneficiary_set:
-            vote_pct = update_pct(vote_pct)
+            weight = update_weight(weight)
 
-        constants.LOGGER.info(f"Voting on {post.authorperm} with {vote_pct}%")
-        post.vote(vote_pct, account=account)
-        bot_comment(post, category, staff_picked)
-        update_sheet(row, previous, current, "Yes")
+        constants.LOGGER.info(f"Voting on {post.authorperm} with {weight}%")
+        # post.vote(weight, account=account)
+        # bot_comment(post, category, staff_picked)
+        update_sheet(row, "Yes")
     except Exception as vote_error:
         constants.LOGGER.error(vote_error)
 
 
+def get_rows():
+    """
+    Return all the rows in the most recent two review worksheets.
+    """
+    previous = constants.PREVIOUS_REVIEWED.get_all_values()
+    current = constants.CURRENT_REVIEWED.get_all_values()
+    rows = previous[1:] + current[1:]
+    return previous, current, rows
+
+
+def points_to_weight(account, points):
+    """
+    Returns the voting weight needed for a vote worth the points equivalence
+    in SBD.
+    """
+    max_SBD = account.get_voting_value_SBD()
+    return 100 * points / max_SBD
+
+
+def handle_comment(contribution):
+    """
+    Finds the moderator's comment in response to the contribution then upvotes
+    and replies to it.
+    """
+    post = Comment(contribution.url)
+    for reply in post.get_replies():
+        if reply.author == contribution.moderator:
+            if not constants.COMMENT_MATCH in reply.body:
+                return False
+            account = Account(constants.ACCOUNT)
+            
+            try:
+                points = constants.CATEGORY_POINTS[contribution.category]
+            except KeyError:
+                points = constants.TASK_REQUEST
+            
+            weight = points_to_weight(account, points)
+            
+            try:
+                # reply.vote(weight, account=account)
+                constants.LOGGER.info(f"Handling comment: {reply.authorperm}")
+                return True
+            except Exception as error:
+                constants.LOGGER.error(f"Handle comment error: {error}")
+                return False
+            
+            break
+
+
+def review_vote():
+    """
+    Finds the first review comment waiting to be upvoted and tries upvoting it.
+    """
+    current = constants.CURRENT_REVIEWED.get_all_values()
+    for row in current:
+        contribution = Contribution(row)
+        if contribution.review_status.lower() == "pending":
+            try:
+                is_handled = handle_comment(contribution)
+                print(is_handled)
+                if is_handled:
+                    update_sheet(contribution, "Yes", False)
+                    time.sleep(20)
+            except Exception as error:
+                constants.LOGGER.error(f"Review vote error: {error}")
+            
+            break
+            
+
 def main():
     """
-    If voting power is 99.75 then it votes on the oldest contribution currently
-    pending.
+    If voting power is > 99.75 then it votes on the oldest contribution
+    currently pending and a review comment made be a moderator.
     """
     voting_power = Account(constants.ACCOUNT).get_voting_power()
     
     constants.LOGGER.info(f"Current voting power: {voting_power}")
-    if voting_power < 99.75:
-        return
+    # if voting_power < 99.75:
+        # return
 
-    previous = constants.PREVIOUS_REVIEWED.get_all_values()
-    current = constants.CURRENT_REVIEWED.get_all_values()
-    rows = previous[1:] + current[1:]
+    review_vote()
+    _, _, rows = get_rows()
 
     response = requests.get("https://utopian.rocks/api/posts?status=pending")
     if len(response.json()) == 0:
@@ -181,20 +258,18 @@ def main():
 
     for contribution in pending:
         for row in rows:
-            voted_for = row[-2].lower()
-            if voted_for != "pending":
+            row = Contribution(row)
+            if row.vote_status.lower() != "pending":
                 continue
 
-            url = row[2]
             # Contribution found in spreadsheet
-            if url == contribution["url"]:
-                post = Comment(url, steem_instance=constants.STEEM)
-                if post.time_elapsed() > timedelta(hours=constants.MINIMUM_AGE):
-                    staff_picked = row[6].lower()
-                    if staff_picked:
-                        vote_update(row, previous, current, True)
+            if row.url == contribution["url"]:
+                post = Comment(row.url, steem_instance=constants.STEEM)
+                if post.time_elapsed() > timedelta(hours=constants.MIN_AGE):
+                    if row.staff_pick.lower() == "yes":
+                        vote_update(row, True)
                     else:
-                        vote_update(row, previous, current)
+                        vote_update(row)
                     return
 
 if __name__ == '__main__':
